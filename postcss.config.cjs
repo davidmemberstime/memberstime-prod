3dbf950e10ded540887a931254c0ccf65074f523
davidmemberstime <david.memberstime@gmail.com>
2026-01-21 14:28:48 +0000

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: {
-    tailwindcss: { config: "./tailwind.config.cjs" },
+    tailwindcss: {
+      config: "./tailwind.config.cjs",
+    },
     autoprefixer: {},
   },
 };
-
