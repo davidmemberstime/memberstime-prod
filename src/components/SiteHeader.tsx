2e6930333b6f198b2095728f95d1e0a1d8cb3cd8
davidmemberstime <david.memberstime@gmail.com>
2026-03-06 11:46:35 +0000

    Update SiteHeader.tsx

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -65,7 +65,7 @@ export default function SiteHeader() {
         {/* Logo */}
        <Link
   href="/"
-  className="relative block h-[110px] w-[110px] shrink-0 overflow-hidden"
+  className="relative block h-[96px] w-[96px] shrink-0 overflow-hidden"
   aria-label="Members Time home"
 >
   <Image
@@ -242,4 +242,5 @@ export default function SiteHeader() {
     </header>
   );
 }
+
 
